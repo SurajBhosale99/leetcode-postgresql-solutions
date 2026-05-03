a156c36b476fa6ac2249e141bd2db735159ab6cb
SurajBhosale99 <surajsbhosale99@gmail.com>
2026-05-03 17:17:37 -0400

    Added second highest salary solutions

---
+++ Easy/second_highest_salary.sql
@@ -0,0 +1,22 @@
+select 
+    distinct salary
+from employees
+order by salary desc
+limit 1 offset 1
+
+-----------------------------------
+
+Select max(salary) as second_higheest_salary
+from employees
+where salary < (select max(salary) from employees) 
+
+
+------------------------------------------
+select salary 
+from(select salary,
+DENSE_RANK() OVER(ORDER BY salary desc) as rnk
+from employees
+)t
+
+
+where rnk=2;
