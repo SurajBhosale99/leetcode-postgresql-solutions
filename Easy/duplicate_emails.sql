7d61373714c9b64228d13312eed726530d670652
SurajBhosale99 <surajsbhosale99@gmail.com>
2026-05-03 17:26:15 -0400

    Added duplicate emails SQL solutions

---
+++ Easy/duplicate_emails.sql
@@ -0,0 +1,11 @@
+select distinct email from employees
+group by email
+having count(*)>1;
+
+-------------------------------------------
+
+SELECT DISTINCT e1.email
+FROM employees e1
+JOIN employees e2
+ON e1.email = e2.email
+AND e1.employee_id != e2.employee_id;
